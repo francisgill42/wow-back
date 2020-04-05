4c9c5b828b1526c9255c95cfa5bd09bb195a3b20
francisgill42 <francisgill42@gmail.com>
2020-04-05 18:46:23 +0500

    added super user using db seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        	$user = \App\User::create([
+        	'name' => 'master',
+        	'email' => 'master@erp.com',
+        	'password' => bcrypt('secret'),
+        ]);
     }
 }
