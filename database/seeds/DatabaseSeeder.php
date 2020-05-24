8f27a9a6e469166a7343945a01aa92972e2b4bc0
francisgill42 <francisgill42@gmail.com>
2020-05-24 12:27:59 +0500

    fixed seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,12 +11,50 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(Level::class);
-        $this->call(SubjectSeeder::class);
+
+            $arr = [
+                'Montessori',
+                'Nursery',
+                'KG 1',
+                'KG 2',
+                'Class 1',
+                'Class 2',
+                'Class 3',
+                'Class 4',
+                'Class 5',
+                'Class 6',
+                'Class 7',
+                'Class 8',
+                'Class 9',
+                'Class 10',
+            ];
+    
+            foreach($arr as $a){
+                App\Level::create(['level' => $a]);
+            }
+
+            $arr = [
+                'English',
+                'Urdu',
+                'Math'
+            ];
+            for ($i=1; $i <= 14 ; $i++) {
+    
+            foreach($arr as $a){
+     
+                    App\Subject::create(['subject' => $a,'level_id' => $i]);
+                }
+    
+            }
+         
+        }
+
+       
+
         // 	$user = \App\User::create([
         // 	'name' => 'master',
         // 	'email' => 'master@erp.com',
         // 	'password' => bcrypt('secret'),
         // ]);
-    }
+    
 }
