e8269102588c58e64cbad1c39c961addccdf10a8
francisgill42 <francisgill42@gmail.com>
2020-05-26 02:25:03 +0500

    added subject functionality

--- app/Http/Controllers/SubjectController.php
+++ app/Http/Controllers/SubjectController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Level;
 use App\Subject;
+use App\Video;
 use Illuminate\Http\Request;
 
 class SubjectController extends Controller
@@ -41,19 +42,20 @@ class SubjectController extends Controller
     }
 
     public function store(Request $request)
-    {
-        // return $request->all();
+    {       
+
         $new_record = Subject::create([
             'level_id' => $request->level_id,
             'subject' => $request->subject
         ]);
 
-        $new_record->level = Level::where('id',$new_record->level_id)->first();
-        return response()->json([
-               'response_status'=>true,
-               'message' => 'record has been created',
-               'new_record' => $new_record
-           ]);
+		$res = ($new_record) 
+        ? [ 'response_status'=>true, 'message' => 'record has been created' ]
+        : [ 'response_status'=>false, 'message' => 'record cannot  create' ];
+
+        return response()->json($res);
+        
+
 
     }
 
@@ -65,8 +67,11 @@ class SubjectController extends Controller
         'subject' => $request->subject
         ]);
 
+        
+
         $level = Subject::find($id);
         $level->level = $level->level;
+        $level[$level->level->level] = $level; 
 
         return response()->json([
         'response_status'=>true,
@@ -78,8 +83,21 @@ class SubjectController extends Controller
 
     public function destroy($id)
     {
-        return (Subject::find($id)->delete()) 
-        ? [ 'response_status' => true,  'message' => 'record has been deleted' ] 
-        : [ 'response_status' => false, 'message' => 'record cannot delete' ];
+
+    	$is_record = Video::where('subject_id',$id)->first();
+
+    	if($is_record){
+    	
+    		Video::where('subject_id',$id)->delete();    	
+    	}
+
+    	return Subject::destroy($id)
+
+    	? [ 'response_status' => true,  'message' => 'record has been deleted' ] 
+		: [ 'response_status' => false, 'message' => 'record cannot delete' ];    		
+
+
+
+
     }
 }
